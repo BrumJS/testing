c905939de1cf094f501ae455dbdb9954c1bb65cd
Ben Paddock <pads@thisispads.me.uk>
2016-10-04 19:03:00 +0100

    Allow tests to run in the browser without tooling

--- test/HelloWorldSpec.js
+++ test/HelloWorldSpec.js
@@ -1,4 +1,9 @@
-var expect = require('chai').expect;
+var expect;
+if(typeof(require) != 'undefined') {
+  expect = require('chai').expect;
+} else {
+  expect = window.chai.expect;
+}
 
 describe('Hello World', function() {
   it('should work', function() {
